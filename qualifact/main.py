1ea725216e975ee535c0ff206e4fb69089ef9cc6
jKyuery <kyugenes17@gmail.com>
2023-03-15 16:55:07 -0500

    refactor

--- qualifact/main.py
+++ qualifact/main.py
@@ -16,10 +16,9 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
 
     print(
-        f'{client_user} is connected to: ' f'{guild.name}(id : {guild.id})'
+        f'{client.user} is connected to: ' f'{guild.name}(id : {guild.id})'
     )
+client.run(TOKEN)
